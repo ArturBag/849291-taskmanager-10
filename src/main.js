1def99a57953c4771517882b23e7819f9c970e03
Артур <artur8888b@gmail.com>
2019-12-09 20:45:15 +0200

    Задание 3. Структуры данных 6. Учебный проект: шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,35 +1,58 @@
 
 import {createMenu} from './components/menu.js';
 import {createContent} from './components/blocks.js';
-import {createSearchForm} from './components/controls.js';
-import {createFilterList} from './components/filters.js';
+import {createFilterTemplate} from './components/controls.js';
+import {createSortingList} from './components/sorting.js';
 import {createBoardTasks} from './components/blocks.js';
-import {createCard} from './components/cards.js';
+import {createTaskEditTemplate} from './components/card-edit.js';
+import {createTaskTemplate} from './components/cards.js';
 import {createButton} from './components/button.js';
-
+import {generateTasks} from './mocks/task.js';
+import {generateFilters} from './mocks/filter.js';
 
 const renderElement = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const CARDS_QTY = 3;
+const CARDS_QTY = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const mainControl = document.querySelector(`.main__control`);
 const mainSection = document.querySelector(`.main`);
 
 renderElement(mainControl, createMenu(), `beforeend`);
-renderElement(mainSection, createSearchForm(), `beforeend`);
+
+const filters = generateFilters();
+
+renderElement(mainSection, createFilterTemplate(filters), `beforeend`);
 renderElement(mainSection, createContent(), `beforeend`);
 
 const board = document.querySelector(`.board`);
 
-renderElement(board, createFilterList(), `beforeend`);
+renderElement(board, createSortingList(), `beforeend`);
+
 renderElement(board, createBoardTasks(), `beforeend`);
 
 const boardTasks = document.querySelector(`.board__tasks`);
+const tasks = generateTasks(CARDS_QTY);
 
-for (let i = 0; i < CARDS_QTY; i++) {
-  renderElement(boardTasks, createCard(), `beforeend`);
-}
+renderElement(boardTasks, createTaskEditTemplate(tasks[0]), `beforeend`);
+
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+tasks.slice(1, showingTasksCount).forEach((task) => renderElement(boardTasks, createTaskTemplate(task), `beforeend`));
 
 renderElement(board, createButton(), `beforeend`);
+
+const loadMoreButton = board.querySelector(`.load-more`);
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+  tasks.slice(prevTasksCount, showingTasksCount)
+    .forEach((task) => renderElement(boardTasks, createTaskTemplate(task), `beforeend`));
+
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});
---
+++ src/mocks/task.js
@@ -0,0 +1,72 @@
+import {COLORS} from '../const.js';
+
+const DescriptionItems = [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`];
+
+const DefaultRepeatingDays = {
+  'mo': false,
+  'tu': false,
+  'we': false,
+  'th': false,
+  'fr': false,
+  'sa': false,
+  'su': false
+};
+
+const Tags = [`homework`, `theory`, `practice`, `intensive`, `keks`];
+
+const getRandomIntegerNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const getRandomArrayItems = (array) => {
+  const randomIndex = [getRandomIntegerNumber(0, array.length)];
+  return array[randomIndex];
+};
+
+
+const getRandomDate = () => {
+  const targetDate = new Date();
+  const sign = Math.random() > 0.5 ? 1 : -1;
+  const diffValue = sign * getRandomIntegerNumber(0, 7);
+
+  targetDate.setDate(targetDate.getDate() + diffValue);
+
+  return targetDate;
+
+};
+
+
+const generateRepeatingDays = () => {
+  return Object.assign({}, DefaultRepeatingDays, {
+    'mo': Math.random() > 0.5,
+  });
+
+};
+
+const generateTags = (tags) => {
+  return tags
+    .filter(() => Math.random() > 0.5)
+    .slice(0, 3);
+};
+
+const generateTask = () => {
+  const dueDate = Math.random() > 0.5 ? null : getRandomDate();
+
+  return {
+    'description': getRandomArrayItems(DescriptionItems),
+    'tags': new Set(generateTags(Tags)),
+    dueDate,
+    'color': getRandomArrayItems(COLORS),
+    'repeatingDays': dueDate ? DefaultRepeatingDays : generateRepeatingDays(),
+    'isFavorite': Math.random() > 0.5,
+    'isArchive': Math.random() > 0.5
+  };
+};
+
+const generateTasks = (count) => {
+  return new Array(count)
+    .fill(``)
+    .map(generateTask);
+};
+
+export {generateTask, generateTasks};
